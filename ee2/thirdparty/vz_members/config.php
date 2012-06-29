a2210790f311e0530476f4579625d3247e2fdb69
Eli Van Zoeren <eli@elivz.com>
2012-06-29 10:04:59 -0700

    Remove non-functional NSM Add-On Updater and change some tabs to spaces

--- ee2/thirdparty/vz_members/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// Info for NSM Addon Updater
-$config['name']='VZ Members';
-$config['version']='1.0';
-$config['nsm_addon_updater']['versions_xml']='http://elivz.com/versions.xml';
--- ee2/thirdparty/vz_members/ft.vz_members.php
+++ ee2/thirdparty/vz_members/ft.vz_members.php
@@ -4,17 +4,17 @@
  * VZ Members Class
  *
  * @author    Eli Van Zoeren <eli@elivz.com>
- * @copyright Copyright (c) 2009-2011 Eli Van Zoeren
+ * @copyright Copyright (c) 2009-2012 Eli Van Zoeren
  * @license   http://creativecommons.org/licenses/by-sa/3.0/ Attribution-Share Alike 3.0 Unported
  */
- 
+
 class Vz_members_ft extends EE_Fieldtype {
 
     public $info = array(
         'name'      => 'VZ Members',
         'version'   => '1.0.5',
     );
-    
+
     /**
      * Fieldtype Constructor
      *
@@ -22,16 +22,16 @@ class Vz_members_ft extends EE_Fieldtype {
     function Vz_members_ft()
     {
         parent::EE_Fieldtype();
-        
+
         // Initialize the cache
         if (!isset($this->EE->session->cache['vz_members']))
         {
-        	$this->EE->session->cache['vz_members'] = array();
+            $this->EE->session->cache['vz_members'] = array();
         }
         $this->cache =& $this->EE->session->cache['vz_members'];
     }
-	
-	
+
+
 	/**
 	 * Install Fieldtype
 	 *
@@ -39,34 +39,34 @@ class Vz_members_ft extends EE_Fieldtype {
     function install()
     {
         // Default field settings
-		return array(
+        return array(
             'member_groups' => array(),
             'mode'          => 'multiple'
         );
     }
-    
+
     var $has_array_data = TRUE;
-    
+
     protected function modes()
     {
-		$this->EE->lang->loadfile('vz_members');
-		
+        $this->EE->lang->loadfile('vz_members');
+
         return array(
             'single'    => $this->EE->lang->line('mode_single'),
             'multiple'  => $this->EE->lang->line('mode_multiple')
         );
     }
-	
-	
-	/**
-	 * Include the JS and CSS files,
-	 * but only the first time
-	 *
-	 */
-	private function _include_jscss()
-	{
-		if ( !isset($this->cache['jscss']) )
-		{
+
+
+    /**
+     * Include the JS and CSS files,
+     * but only the first time
+     *
+     */
+    private function _include_jscss()
+    {
+        if ( !isset($this->cache['jscss']) )
+        {
             $this->EE->cp->add_to_head('<style type="text/css">
                 div.vz_members_group { float:left; height:18px; line-height:18px !important; margin:3px 10px 7px 0; font-size:12px; }
                 label.vz_member { float:left; height:14px; line-height:14px !important; margin:3px 10px 7px 0; padding:2px 10px; border:1px solid #B6C0C2; -moz-border-radius:9px; border-radius:9px; text-shadow:0 1px #fff; background:#ebf1f7; -webkit-box-shadow:inset 0 2px 3px rgba(255,255,255,0.8); -moz-box-shadow:inset 0 2px 3px rgba(255,255,255,0.8); box-shadow:inset 0 2px 3px rgba(255,255,255,0.8); cursor:pointer; white-space:nowrap; }
@@ -81,12 +81,12 @@ class Vz_members_ft extends EE_Fieldtype {
                     });
                 });'
             );
-			
-			$this->cache['jscss'] = TRUE;
-		}
-	}
-	
-	
+
+            $this->cache['jscss'] = TRUE;
+        }
+    }
+
+
     /**
     * Member Groups Select
     */
@@ -101,7 +101,7 @@ class Vz_members_ft extends EE_Fieldtype {
                 FROM exp_member_groups
                 WHERE site_id = 1
                 ")->result_array();
-            
+
             // We need it in key-value form for the select helper functions
             foreach ($result as $item)
             {
@@ -109,42 +109,42 @@ class Vz_members_ft extends EE_Fieldtype {
             }
             $this->cache['groups']['all'] = $member_groups;
         }
-        
+
         return $this->cache['groups']['all'];
     }
-  
-    
+
+
     /**
      * Create the settings ui
      */
     private function _get_settings($settings)
     {
-		$this->EE->lang->loadfile('vz_members');
-		$this->EE->load->helper('form');
-        
+        $this->EE->lang->loadfile('vz_members');
+        $this->EE->load->helper('form');
+
         $mode = isset($settings['mode']) ? $settings['mode'] : 0;
         $row1 = array(
             $this->EE->lang->line('mode_label_cell'),
             form_dropdown('mode', $this->modes(), $mode)
         );
-        
+
         $member_groups = isset($settings['member_groups']) ? $settings['member_groups'] : 0;
-		$row2 = array(
+        $row2 = array(
             $this->EE->lang->line('member_groups_label_cell'),
             form_multiselect('member_groups[]', $this->_get_member_groups(), $member_groups)
         );
-        
+
         return array( $row1, $row2 );
     }
-    
-    
+
+
     /**
      * Display Field Settings
      */
     function display_settings($field_settings)
     {
         $settings_array = $this->_get_settings($field_settings);
-        
+
         $this->EE->table->add_row($settings_array[0]);
         $this->EE->table->add_row($settings_array[1]);
     }
@@ -152,51 +152,51 @@ class Vz_members_ft extends EE_Fieldtype {
 
     function save_settings($data)
     {
-    	return array(
-    		'mode' => $this->EE->input->post('mode'),
-    		'member_groups' => $this->EE->input->post('member_groups')
-    	);
+        return array(
+            'mode' => $this->EE->input->post('mode'),
+            'member_groups' => $this->EE->input->post('member_groups')
+        );
     }
-	
-    
-	/**
-	 * Display Cell Settings
-	 */
-	function display_cell_settings($cell_settings)
-	{
-		return $this->_get_settings($cell_settings);
-	}
-	
-	
-	/**
-	 * Create the user checkboxes or select list
-	 */
+
+
+    /**
+     * Display Cell Settings
+     */
+    function display_cell_settings($cell_settings)
+    {
+        return $this->_get_settings($cell_settings);
+    }
+
+
+    /**
+     * Create the user checkboxes or select list
+     */
     function _create_user_list($field_name, $selected_members)
     {
-		$this->EE->load->helper('form');
-		
-		// Not sure why this is needed... Safecracker doesn't return the correct values for $this->settings
-		if (isset($this->settings['field_settings']) && $field_settings = @unserialize(base64_decode($this->settings['field_settings'])))
-		{
-			$this->settings = array_merge($this->settings, $field_settings);
-		}
-		
-		$member_groups = $this->settings['member_groups'];
-		$mode = $this->settings['mode'];
-        
+        $this->EE->load->helper('form');
+
+        // Not sure why this is needed... Safecracker doesn't return the correct values for $this->settings
+        if (isset($this->settings['field_settings']) && $field_settings = @unserialize(base64_decode($this->settings['field_settings'])))
+        {
+            $this->settings = array_merge($this->settings, $field_settings);
+        }
+
+        $member_groups = $this->settings['member_groups'];
+        $mode = $this->settings['mode'];
+
         // If there are no member groups selected, don't bother
         if (empty($member_groups))
         {
             $this->EE->lang->loadfile('vz_members');
             return '<div class="highlight">' . $this->EE->lang->line('no_member_groups') . '</div>';
         }
-        
+
         // Flatten the list of member groups to csv
         if (is_array($member_groups))
         {
             $member_groups = implode(',', $member_groups);
         }
-	    
+
         // Get the members in the selected member groups
         if ( !isset($this->cache['in_groups'][$member_groups]) )
         {
@@ -204,7 +204,7 @@ class Vz_members_ft extends EE_Fieldtype {
                 SELECT
                     exp_members.member_id AS member_id,
                     exp_members.screen_name AS screen_name,
-                    exp_member_groups.group_title AS group_title, 
+                    exp_member_groups.group_title AS group_title,
                     exp_member_groups.group_id AS group_id
                 FROM
                     exp_members
@@ -212,28 +212,28 @@ class Vz_members_ft extends EE_Fieldtype {
                     exp_member_groups
                 ON
                     exp_members.group_id = exp_member_groups.group_id
-                WHERE 
+                WHERE
                     exp_member_groups.group_id IN ($member_groups) AND exp_member_groups.site_id = 1
-                ORDER BY 
+                ORDER BY
                     exp_member_groups.group_id ASC, exp_members.screen_name ASC
             ")->result_array();
         }
         $members = $this->cache['in_groups'][$member_groups];
-    
+
         $r = '';
         $current_group = 0;
-        
+
         // We want the selected members as an array
         if (!is_array($selected_members))
         {
             $selected_members = explode('|', $selected_members);
         }
-        
+
         if ($mode == 'single')
         {
             // Get the first selected member if there are more than one
             $selected_members = array_shift($selected_members);
-            
+
             // Construct the select box markup
             $r = '<select name="' . $field_name . '">';
             $r .= '<option value=""' . (!$selected_members ? ' selected="selected"' : '') . '>&mdash;</option>' . NL;
@@ -245,14 +245,14 @@ class Vz_members_ft extends EE_Fieldtype {
                     // Output the group header
                     if ($current_group) $r .= '</optgroup>' . NL;
                     $r .= '<optgroup label="'.$member['group_title'].'">' . NL;
-                    
+
                     // Set the new current group
                     $current_group = $member['group_id'];
                 }
-            
+
                 // Output the option
                 $r .= '<option value="' . $member['member_id'] . '"'
-                    . ($member['member_id'] == $selected_members ? ' selected="selected"' : '') . '>' 
+                    . ($member['member_id'] == $selected_members ? ' selected="selected"' : '') . '>'
                     . $member['screen_name'] . '</option>' . NL;
             }
             $r .= '</optgroup>';
@@ -262,41 +262,41 @@ class Vz_members_ft extends EE_Fieldtype {
         {
             foreach ($members as $member)
             {
-            	// If we are moving on to a new group
-            	if ($current_group != $member['group_id'])
-            	{
+                // If we are moving on to a new group
+                if ($current_group != $member['group_id'])
+                {
                     // Set the current group
                     $current_group = $member['group_id'];
-                    
+
                     // Output the group header
                     $r .= '<div style="clear:left"></div>';
                     $r .= '<div class="defaultBold vz_members_group">'.$member['group_title'].':</div>';
-            	}
-            
+                }
+
                 // Is it selected?
-            	$checked = (in_array($member['member_id'], $selected_members)) ? 1 : 0;
-        	  
+                $checked = (in_array($member['member_id'], $selected_members)) ? 1 : 0;
+
                 // Output the checkbox
                 $r .= '<label class="vz_member' . ($checked ? ' checked' : '') . '">'
                     . form_checkbox($field_name.'[]', $member['member_id'], $checked)
                     . $member['screen_name']
                     . '</label>';
-        	}
-        	
+            }
+
             // Fool the form into working
             $r .= form_hidden($field_name.'[]', 'temp');
-            
+
             // Make it pretty
             $this->_include_jscss();
-            
+
             // Clear the floats
             $r .= '<div style="clear:left"></div>';
         }
-        
+
         return $r;
     }
-  
-  
+
+
     /**
      * Display Field
      */
@@ -304,8 +304,8 @@ class Vz_members_ft extends EE_Fieldtype {
     {
         return $this->_create_user_list($this->field_name, $field_data);
     }
-	
-    
+
+
     /**
      * Display Cell
      */
@@ -314,22 +314,22 @@ class Vz_members_ft extends EE_Fieldtype {
         return $this->_create_user_list($this->cell_name, $cell_data);
     }
 
-    
+
     /**
      * Save Field
      */
     function save($field_data)
     {
-    	// Remove the temporary element
-    	if (is_array($field_data))
-    	{
-    	   @array_pop($field_data);
-    	   $field_data = implode('|', $field_data);
-    	}
-    	return $field_data;
+        // Remove the temporary element
+        if (is_array($field_data))
+        {
+           @array_pop($field_data);
+           $field_data = implode('|', $field_data);
+        }
+        return $field_data;
     }
-    
-    
+
+
     /**
      * Save Cell
      */
@@ -349,19 +349,19 @@ class Vz_members_ft extends EE_Fieldtype {
         if (!$member_list) $member_list = -1;
         $sort = (strtolower($sort) == 'desc') ? 'DESC' : 'ASC';
         $orderby = ($orderby == 'username' || $orderby == 'screen_name' || $orderby == 'group_id') ? $orderby : 'member_id';
-        
+
         // Only hit the database once per pageload
         if ( !isset($this->cache['members'][$member_list][$orderby][$sort]) )
         {
             // Get the names of the members
             $this->cache['members'][$member_list][$orderby][$sort] = $this->EE->db->query("
                 SELECT member_id, group_id, username, screen_name
-                FROM exp_members 
+                FROM exp_members
                 WHERE member_id IN ($member_list)
                 ORDER BY $orderby $sort
                 ")->result_array();
         }
-        
+
         return $this->cache['members'][$member_list][$orderby][$sort];
     }
 
@@ -373,16 +373,16 @@ class Vz_members_ft extends EE_Fieldtype {
         if (!$tagdata) // Single tag
         {
             return $field_data;
-    	}
-    	else // Tag pair
-    	{
+        }
+        else // Tag pair
+        {
             // Set default parameters
             $orderby = isset($params['orderby']) ? $params['orderby'] : '';
             $sort = isset($params['sort']) ? $params['sort'] : '';
 
             // Get the member info
             $members = $this->_get_member_names($field_data, $orderby, $sort);
-            
+
             $variables = array();
             foreach ($members as $member)
             {
@@ -394,17 +394,17 @@ class Vz_members_ft extends EE_Fieldtype {
                     'screen_name' => $member['screen_name']
                 );
             }
-            
+
             $output = $this->EE->TMPL->parse_variables($tagdata, $variables);
-            
+
             // Backsapce parameter
             if (isset($params['backspace']))
             {
                 $output = substr($output, 0, -$params['backspace']);
             }
-            
+
             return $output;
-    	}
+        }
     }
 
 
@@ -419,75 +419,75 @@ class Vz_members_ft extends EE_Fieldtype {
 
         // Get the member info
         $members = $this->_get_member_names($field_data, $orderby, $sort);
-        
+
         // Initialize the member_names array
         $member_nammes = array();
-        
+
         // Put the names in an array
         foreach ($members as $member)
         {
             $member_names[] = $member['screen_name'];
         }
-        
+
         // Output the list
         $separator = isset($params['separator']) ? $params['separator'] : ', ';
         return implode($separator, $member_names);
     }
-  
-  
+
+
     /**
     * Checks the intersection between the selected members and a
-    * member or list of members 
+    * member or list of members
     */
     function _is_allowed($params, $field_data)
     {
         $allowed = explode('|', $field_data);
-        
+
         if ( isset($params['members']) )
         {
             $candidates = explode('|', $params['members']);
         }
-        
+
         if ( isset($params['groups']) )
         {
             // Get all the users in those groups
             if ( !isset($this->cache['groups'][$params['groups']]) )
             {
                 $this->cache['groups'][$params['groups']] = $this->EE->db->query("
-                	SELECT member_id
-                	FROM exp_members 
-                	WHERE group_id IN (".$params['groups'].")
+                    SELECT member_id
+                    FROM exp_members
+                    WHERE group_id IN (".$params['groups'].")
                     ")->result_array();
             }
             $supers = $this->cache['groups'][$params['groups']];
-            
+
             // Separate out the member_ids
             foreach ($supers as $super)
             {
                 $candidates[] = $super['member_id'];
             }
         }
-        
+
         // Get the current user, if necessary
         if ( isset($params['current_member']) )
         {
             $candidates[] = $this->EE->session->userdata['member_id'];
         }
-        
+
         // Are there any matches between the two?
         return (count(array_intersect($candidates, $allowed)) > 0);
     }
-    
+
     function replace_is_allowed($data, $params = array(), $tagdata = FALSE)
     {
         return $this->_is_allowed($params, $data) ? TRUE : FALSE;
     }
-    
+
     function replace_is_not_allowed($data, $params = array(), $tagdata = FALSE)
     {
         return $this->_is_allowed($params, $data) ? FALSE : TRUE;
     }
-  
+
 }
 
 /* End of file ft.vz_members.php */
